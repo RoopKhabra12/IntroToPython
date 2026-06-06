db51bfbeeade69dac2c8503b1c607d21b1aa8b64
RoopKhabra12 <roopkhabra@gmail.com>
2026-06-06 12:59:40 -0400

    Add files via upload

---
+++ 1.2 C.W.py
@@ -0,0 +1,33 @@
+import turtle # we need this to make turtles
+lisa= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+lisa.forward(100) # this forward command asks lisa to move 100 steps forward
+lisa.left(90) # this left command asks lisa to turn 90 degrees left
+lisa.forward(100)  # this forward command asks lisa to move 100 steps forward
+lisa.left(90)
+lisa.forward(100)
+lisa.left(90)
+lisa.forward(100)  # this forward command asks lisa to move 100 steps forward
+lisa.left(90)
+lisa.penup() # she lifts her pen up here 
+
+##lisa.color("pink")   # and her pen will become pink
+##lisa.backward(100)  # this backward command asks lisa to move 100 steps backward
+##lisa.right(90)
+##lisa.color("blue") # lisa and her pen will become blue
+##lisa.backward(100)  # this backward command asks lisa to move 100 steps backward
+##lisa.color("red") # lisa and her pen will become red
+##lisa.right(90)
+##lisa.backward(100)  # this backward command asks lisa to move 100 steps backward
+##lisa.right(90)
+##lisa.backward(100)  # this backward command asks lisa to move 100 steps backward
+##lisa.color("yellow")
+##lisa.pensize(0) # lisa's pen will be thin
+##lisa.forward(100) # so this line will be thin too
+##lisa.pensize(10)  # lisa's pen will be thicker now
+##lisa.forward (100) # so this line will be thicker
+
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.penup() # she lifts her pen up here
+##lisa.forward(100) # no line is drawn because the pen is up
+##lisa.pendown() # she puts the pen back down
+##lisa.forward(100) # a line is drawn 
