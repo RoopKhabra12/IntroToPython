c0036711956c3badfb78868e421a477e68b2794d
RoopKhabra12 <roopkhabra@gmail.com>
2026-06-06 11:50:36 -0400

    Add files via upload

---
+++ 1.1 H.W.py
@@ -0,0 +1,90 @@
+###Q1 - Draw House
+####import turtle   # this allows us to use turtles
+####shelly= turtle.Turtle() # read this like "shelly is a turtle"
+####shelly.left(120) 
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(120)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(120)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.right(90)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.right(90)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.right(90)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.backward(100) # the number 100 here means 100 steps
+####shelly.right(90)
+####shelly.forward(40) # the number 40 here means 40 steps
+####shelly.left(90)
+####shelly.forward(40) # the number 40 here means 40 steps
+####shelly.right(90)
+####shelly.forward(20) # the number 20 here means 20 steps
+####shelly.left(90)
+####shelly.backward(40) # the number 40 here means 40 steps
+
+###Q2 Pentagon
+##import turtle
+##shelly = turtle.Turtle() # read this like "shelly is a turtle"
+##shelly.forward(100) # the number 100 here means 100 steps
+##shelly.left(72)
+##shelly.forward(100) # the number 100 here means 100 steps
+##shelly.left(72)
+##shelly.forward(100) # the number 100 here means 100 steps
+##shelly.left(72)
+##shelly.forward(100) # the number 100 here means 100 steps
+##shelly.left(72)
+##shelly.forward(100) # the number 100 here means 100 steps
+##shelly.left(72)
+
+###Q3 Hexagon
+####import turtle
+####shelly = turtle.Turtle() # read this like "shelly is a turtle"
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+####shelly.forward(100) # the number 100 here means 100 steps
+####shelly.left(60)
+
+###Q4 Staircase
+##import turtle
+##shelly = turtle.Turtle() #  read this like "shelly is a turtle"
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.left(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.right(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.left(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.right(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.left(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.right(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##shelly.left(90)
+##shelly.forward(50) # the number 50 here means 50 steps
+##
+
+##Q5 Mountain Range
+import turtle
+shelly = turtle.Turtle() #  read this like "shelly is a turtle"
+shelly.left(60)
+shelly.forward(100) # the number 100 here means 100 steps
+shelly.right(120)
+shelly.forward(100) # the number 100 here means 100 steps
+shelly.left(120)
+shelly.forward(100) # the number 100 here means 100 steps
+shelly.right(120)
+shelly.forward(100) # the number 100 here means 100 steps
+shelly.left(120)
+shelly.forward(100) # the number 100 here means 100 steps
+shelly.right(120)
+shelly.forward(100) # the number 100 here means 100 steps
