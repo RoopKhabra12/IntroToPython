a1e61cc85453b47fe263fed53ea9840697801797
RoopKhabra12 <roopkhabra@gmail.com>
2026-06-13 12:30:30 -0400

    Update 1.2 C.W.py

--- 1.2 C.W.py
+++ 1.2 C.W.py
@@ -31,3 +31,54 @@ lisa.penup() # she lifts her pen up here
 ##lisa.forward(100) # no line is drawn because the pen is up
 ##lisa.pendown() # she puts the pen back down
 ##lisa.forward(100) # a line is drawn 
+
+###Q1 pensize0 
+##import turtle # we need this to make turtles
+##lisa= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+##lisa.pensize(0) # lisa's pen will be thin
+##lisa.forward(100)  # this forward command asks lisa to move 100 steps forward
+
+###Q2
+##import turtle # we need this to make turtles
+##lisa= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+##lisa.color("green")
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.left(120)
+##lisa.forward(100) # a line is drawn
+##lisa.left(120)
+##lisa.forward(100) # a line is drawn
+
+#Q3
+import turtle # we need this to make turtles
+##lisa= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+##lisa.color("green")
+##lisa.pensize(5)  # lisa's pen will be thicker now
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.left(120)
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.left(120)
+##lisa.forward(100) # a line is drawn
+
+###Q4
+##leo= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+##leo.penup() # she lifts her pen up here
+##leo.goto(15,15) # lisa's pen is always down at the start
+##leo.pendown()  # she lifts her pen down here
+##leo.pensize(5)  # lisa's pen will be thicker now
+##leo.color("green")
+##leo.forward(100) # lisa's pen is always down at the start
+##leo.forward(100) # lisa's pen is always down at the start
+##leo.left(120)
+##leo.forward(100) # lisa's pen is always down at the start
+##leo.left(120)
+##leo.forward(100) # lisa's pen is always down at the start
+##
+###Q5
+##lisa= turtle.Turtle () # read this as "lisa is a turtle". We need a turtle in order to draw
+##lisa.color("pink")
+##lisa.pensize(10)  # lisa's pen will be thicker now
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.left(120)
+##lisa.forward(100) # lisa's pen is always down at the start
+##lisa.left(120)
+##lisa.forward(100) # lisa's pen is always down at the start
