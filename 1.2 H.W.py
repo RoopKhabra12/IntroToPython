e02f5a66f1eca501f355f2199c679bc860936be6
RoopKhabra12 <roopkhabra@gmail.com>
2026-06-18 18:23:55 -0400

    Update 1.2 H.W.py

--- 1.2 H.W.py
+++ 1.2 H.W.py
@@ -1,1 +1,19 @@
+import turtle # we need this to make turtles
+roop= turtle.Turtle () # read this as "roop is a turtle". We need a turtle in order to draw
+roop.pensize(5) 
+roop.color("pink")
+roop.left(90) 
+roop.forward(200)
+roop.right(90)
+roop.forward(100)
+roop.right(90)
+roop.forward(75)
+roop.right(90)
+roop.forward(100)
+roop.left(130)
+roop.forward(150)
+roop.penup() # she lifts her pen up here
+roop.pendown() # she puts the pen back down
+roop.color("grey")
+roop.pensize(10) 
 
