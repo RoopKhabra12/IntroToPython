7b71c049d9c588f77fa52f28f692b5fa55a79927
RoopKhabra12 <roopkhabra@gmail.com>
2026-06-20 12:54:11 -0400

    Update 1.3 C.W.py

--- 1.3 C.W.py
+++ 1.3 C.W.py
@@ -1,1 +1,63 @@
+##import turtle # we need this to make turtles
+##jayden = turtle.Turtle()
+##jayden.pensize(10)
+##jayden.penup() #pick up the pen to move without drawing
+##jayden.fillcolor("light blue")
+##jayden.begin_fill()
+##jayden.backward(200)
+##jayden.pendown() #put pen down to draw again
+##jayden.pencolor("blue") #this is how we change colour
+##jayden.forward(100)
+##jayden.left(120)
+##jayden.pencolor("yellow")
+##jayden.forward(100)
+##jayden.left(120)
+##jayden.pencolor("brown") #this is how we change colour
+##jayden.forward(100)
+##jayden.left(120)
+##jayden.end_fill()
 
+##jayden = turtle.Turtle()
+##jayden.pensize(12)
+##jayden.penup() #pick up the pen to move without drawing
+##jayden.fillcolor("blue")
+##jayden.begin_fill()
+##jayden.forward(200)
+##jayden.pendown() #put pen down to draw again
+##jayden.pencolor("blue") #this is how we change colour
+##jayden.forward(100)
+##jayden.left(120)
+##jayden.forward(100)
+##jayden.left(120)
+##jayden.forward(100)
+##jayden.end_fill()
+
+##import turtle
+##sanjay = turtle.Turtle()
+##sanjay.color("blue")
+##sanjay.begin_fill()
+##sanjay.forward(100)
+##sanjay.left(90)
+##sanjay.forward(100)
+##sanjay.end_fill()
+##sanjay.color("red")
+##sanjay.begin_fill()
+##sanjay.left(90)
+##sanjay.forward(100)
+##sanjay.left(90)
+##sanjay.forward(100)
+##sanjay.end_fill()
+
+##import turtle # we need this to make turtles
+##jayden = turtle.Turtle()
+##jayden.color("green")
+##jayden.begin_fill()
+##jayden.circle(50) # this number decides the circle size
+##jayden.end_fill()
+
+##import turtle # we need this to make turtles
+##jayden = turtle.Turtle()
+##jayden.fillcolor("green") # the inside will be green
+##jayden.pencolor("black") # the border is black
+##jayden.circle(200)
+##jayden.end_fill()
