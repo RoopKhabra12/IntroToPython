f580f89a0a4863ce1fdc35ac9505b184ae822774
RoopKhabra12 <roopkhabra@gmail.com>
2026-07-11 11:59:49 -0400

    Create 1.4 C.W.py

---
+++ 1.4 C.W.py
@@ -0,0 +1,7 @@
+##import turtle
+##julia = turtle.Turtle()
+##julia.fillcolor("pink") # sets fill colour to pink
+##julia.pencolor("black") # sets pen colour to black
+##julia.begin_fill() # the shape between here and end_fill
+##julia.circle(100) # will be filled in pink
+##julia.end_fill() # we've drawn a circle of size 100
