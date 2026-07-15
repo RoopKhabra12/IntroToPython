20e4e96afa088d21322397be7750cd923d71916d
RoopKhabra12 <roopkhabra@gmail.com>
2026-07-15 18:19:23 -0400

    Update 1.4 C.W.py

--- 1.4 C.W.py
+++ 1.4 C.W.py
@@ -6,7 +6,21 @@
 ##julia.circle(100) # will be filled in pink
 ##julia.end_fill() # we've drawn a circle of size 100
 
+import turtle
+chris = turtle.Turtle()
+chris.write("(0,0)")
+chris.penup()
+chris.goto(-300,0) # our turtle will go to this location
+chris.write("(-300,0)")
+chris.goto(300,0) # goto always needs 2 numbers, X then Y
+chris.write("(300,0)")
+chris.goto(0,-300)
+chris.write("(0,-300)")
+chris.goto(0,300)
+chris.write("(0,300)")
 
+#Q1
+Yellow (0,100),Brown (-250,250), Neon Blue (-150,-250) Neon Pink (0,200)
 
 
 
