e2509036ea4882c7087c6b4a4ad7204e0d7e174c
RoopKhabra12 <roopkhabra@gmail.com>
2026-07-18 12:12:08 -0400

    Update 1.4 H.W.py

--- 1.4 H.W.py
+++ 1.4 H.W.py
@@ -1,1 +1,73 @@
+##import turtle
+##chris = turtle.Turtle()
+### Step 1
+##chris.pencolor("blue")
+##chris.pensize(3)
+### Step 2
+##chris.penup()
+##chris.goto(0,-300)
+### Step 3
+##chris.pendown()
+##chris.circle(100)
+### Step 4
+##chris.penup()
+##chris.goto(0,-100)
+### Step 5
+##chris.pendown()
+##chris.circle(70)
+### Step 6
+##chris.penup()
+##chris.goto(0,40)
+### Step 7
+##chris.pendown()
+##chris.circle(40)
+### Step 8
+##chris.pencolor("brown")
+##chris.pensize(5)
+### Step 9
+##chris.penup()
+##chris.goto(-70,-30)
+### Step 10 
+##chris.setheading(180)
+##chris.pendown()
+##chris.forward(100)
+### Step 11
+##chris.penup()
+##chris.goto(70,-30)
+### Step 12
+##chris.setheading(0)
+##chris.pendown()
+##chris.forward(100)
+### Scarf
+##chris.penup()
+##chris.goto(0, 40)
+##chris.setheading(0)
+##chris.pendown()
+##chris.pencolor("red")
+##chris.pensize(20)
+##chris.circle(40,45)
+##chris.penup()
+##chris.circle(40,270)
+##chris.pendown()
+##chris.circle(40,45)
+##chris.setheading(300)
+##chris.forward(40)
+##chris.setheading(240)
+##chris.forward(60)
+### Buttons 
+##chris.penup()
+##chris.goto(0,-60)
+##chris.pendown()
+##chris.pencolor("black")
+##chris.pensize(6)
+##chris.dot(12) 
+##chris.penup()
+##chris.goto(0,-90) 
+##chris.pendown()	
+##chris.dot(12)
+##chris.penup()
+##chris.goto(0,-120) 
+##chris.pendown()
+##chris.dot(12) 
+##turtle.done()
 
