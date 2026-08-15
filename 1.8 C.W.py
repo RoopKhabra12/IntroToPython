90cb23e39f4113414633eef9036eccaf84361b51
RoopKhabra12 <roopkhabra@gmail.com>
2026-08-15 10:33:10 -0400

    Update 1.8 C.W.py

--- 1.8 C.W.py
+++ 1.8 C.W.py
@@ -15,3 +15,27 @@ t.pendown()
 t.forward(30)
 t.penup() # draw next tick by repeating
 t.goto(0,0)
+
+t.setheading(0)    # 3 o'clock
+t.setheading(30)   # 1 o'clock
+t.setheading(60)   # 2 o'clock
+t.setheading(90)   # 12 o'clock
+t.setheading(180)  # 9 o'clock
+t.setheading(270)  # 6 o'clock
+
+import datetime
+currentMinute = datetime.datetime.now().minute
+currentHour = datetime.datetime.now().hour
+t.penup()
+t.goto(0,0)
+t.color("red")
+t.pendown()
+t.setheading(90)
+t.right(currentHour * 360/12)
+t.forward(100)
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.setheading(90)
+t.right(currentMinute * 360/60)
+t.forward(150)
